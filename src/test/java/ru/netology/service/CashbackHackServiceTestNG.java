c9f68a049022a134539ba434f0f1363c294dd066
Aliya Voronkina <atleugabilova@mail.ru>
2025-10-07 12:06:44 +0500

    testng fixes

--- src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
 import org.testng.annotations.Test;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTestNG {
 
